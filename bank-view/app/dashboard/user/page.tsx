d04e9a1bbec384d90cac8d3a92144ac986d41e0f
gossing <withgossing@outlook.com>
2024-10-13 15:40:02 +0900

    header, footer layout 적용

---
+++ bank-view/app/dashboard/user/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header>user header</header>
+      <main>user main</main>
+      <footer>user footer</footer>
+    </div>
+  );
+}
