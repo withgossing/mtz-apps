685917f4867f5e4bed7309277393dc708f7a5ba9
gossing <withgossing@outlook.com>
2024-11-03 03:01:46 +0900

    was 프로젝트 개선

--- bank-api/src/domain/user/dto/create-user.dto.ts
+++ bank-api/src/domain/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+  @ApiProperty({ description: "사용자 ID" })
+  userId: string;
+
+  @ApiProperty({ description: "사용자 이름" })
+  userName: string;
+
+  @ApiProperty({ description: "사용자 비밀번호" })
+  password: string;
+
+  @ApiProperty({ description: "사용자 이메일주소" })
+  email: string;
+}
